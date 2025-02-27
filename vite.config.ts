92e01737d783a87c438000d6e7096753263fec6b
Nikolai Topping <ntop@stibosystems.com>
2025-02-27 17:48:46 +0100

    Set correct base path

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/numberplate-fun/',
+  base: '/numberplate-fun-times/',
   plugins: [
     react(),
     VitePWA({
